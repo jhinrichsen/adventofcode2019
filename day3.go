65db7c85e6c66f941afac55947ad923129427749
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2019-12-31 01:35:08 +0100

    feat: add day 3 part 2

--- day3.go
+++ day3.go
@@ -22,14 +22,15 @@ const (
 	Left
 )
 
-// Day3 computes the minimal manhattan distance of two crossing wires
-func Day3(wires []string) (int, error) {
+// Day3Part1 computes the minimal manhattan distance of two crossing wires
+func Day3Part1(wires []string) (int, error) {
 	maxX, maxY, err := MaxSize(wires)
 	if err != nil {
 		return 0, err
 	}
 
-	b := Board(1+maxX*2, 1+maxY*2)
+	// Create a board that is large enough to always fit the wiring
+	b := Board((1+maxX)*2, (1+maxY)*2)
 	if err := Walk(b, wires); err != nil {
 		return 0, err
 	}
@@ -178,4 +179,86 @@ func Walk(b [][]uint, wires []string) error {
 		}
 	}
 	return nil
+}
+
+// Point holds a (x/y) position
+type Point struct {
+	x, y int
+}
+
+// Marker holds steps needed to reach position (x/y)
+type marker map[Point]int
+
+// Store will save the first, and only the first, number of steps for (x/y)
+func storeOnce(m marker, x, y, steps int) {
+	p := Point{x, y}
+	// do nothing if position already visited
+	if _, ok := m[p]; ok {
+		return
+	}
+	m[p] = steps
+}
+
+// Day3Part2 computes the minimal combined steps for intersections
+func Day3Part2(wires []string) (int, error) {
+	boards := make([]marker, len(wires))
+	for i := 0; i < len(boards); i++ {
+		boards[i] = make(marker)
+	}
+
+	// Transform wires into marker maps
+	for i, wire := range wires {
+		board := boards[i]
+		x, y, steps := 0, 0, 0
+		ws := strings.Split(wire, ",")
+		for _, w := range ws {
+			d, n, err := Parse(w)
+			if err != nil {
+				return -1, err
+			}
+			for j := 0; j < n; j++ {
+				storeOnce(board, x, y, steps)
+				steps++
+				// Next time i write this part i will decode the
+				// RLE (run length encoding) into distinct (x/y)
+				// deltas - turtle graphics ;-)
+				switch d {
+				case Right:
+					x++
+				case Left:
+					x--
+				case Up:
+					y++
+				case Down:
+					y--
+				}
+			}
+		}
+	}
+
+	// Find all intersections, and the sum of steps
+	intersections := make(marker)
+	// use wiring of first board as reference
+	for refpos, refsteps := range boards[0] {
+		// ignore center point (0/0)
+		if refpos.x == 0 && refpos.y == 0 {
+			continue
+		}
+		// check for collision
+		for _, board := range boards[1:] {
+			if steps, ok := board[refpos]; ok {
+				intersections[refpos] = refsteps + steps
+			}
+		}
+	}
+
+	// find lowest intersection
+	min := math.MaxInt32
+	for _, sum := range intersections {
+		if sum < min {
+			min = sum
+		}
+	}
+
+	return min, nil
 }
--- day3_test.go
+++ day3_test.go
@@ -6,26 +6,27 @@ import (
 )
 
 var day3Examples = []struct {
-	in  []string
-	out int
+	in    []string
+	part1 int
+	part2 int
 }{
 	{[]string{
 		"R75,D30,R83,U83,L12,D49,R71,U7,L72",
 		"U62,R66,U55,R34,D71,R55,D58,R83",
-	}, 159},
+	}, 159, 610},
 	{[]string{
 		"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
 		"U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
-	}, 135},
+	}, 135, 410},
 }
 
 // Example stopped working after Part 1 - no clue
 func Day3Part1Example(t *testing.T) {
 	for i, tt := range day3Examples {
-		id := fmt.Sprintf("Example #%d", i)
+		id := fmt.Sprintf("Part 1 Example #%d", i)
 		t.Run(id, func(t *testing.T) {
-			want := tt.out
-			got, err := Day3(tt.in)
+			want := tt.part1
+			got, err := Day3Part1(tt.in)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -71,7 +72,7 @@ func TestDay3Part1(t *testing.T) {
 		t.Fatal(err)
 	}
 	want := 248
-	got, err := Day3(lines)
+	got, err := Day3Part1(lines)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -86,6 +87,48 @@ func BenchmarkDay3Part1(b *testing.B) {
 		b.Fatal(err)
 	}
 	for i := 0; i < b.N; i++ {
-		Day3(lines)
+		Day3Part1(lines)
+	}
+}
+
+func TestDay3Part2Example(t *testing.T) {
+	for i, tt := range day3Examples {
+		id := fmt.Sprintf("Part 2 Example #%d", i)
+		t.Run(id, func(t *testing.T) {
+			want := tt.part2
+			got, err := Day3Part2(tt.in)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if want != got {
+				t.Fatalf("%s: want %d but got %d", id,
+					want, got)
+			}
+		})
+	}
+}
+
+func TestDay3Part2(t *testing.T) {
+	lines, err := Lines("testdata/day3.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := 28580
+	got, err := Day3Part2(lines)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func BenchmarkDay3Part2(b *testing.B) {
+	lines, err := Lines("testdata/day3.txt")
+	if err != nil {
+		b.Fatal(err)
+	}
+	for i := 0; i < b.N; i++ {
+		Day3Part2(lines)
 	}
 }
