36175804c9865be92478afb1e9c6ae56e49edc36
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2020-02-18 08:38:39 +0100

    test: add benchmark

--- day6_test.go
+++ day6_test.go
@@ -2,37 +2,76 @@ package adventofcode2019
 
 import "testing"
 
-func day6FromFile(t *testing.T, filename string) Day6 {
+func day6FromFile(filename string) (Day6, error) {
 	ss, err := Lines(filename)
 	if err != nil {
-		t.Fatal(err)
+		return Day6{}, err
 	}
 	d, err := NewDay6(ss)
+	if err != nil {
+		return d, err
+	}
+	return d, nil
+}
+
+func example1() (Day6, error) {
+	return day6FromFile("testdata/day6_example.txt")
+}
+
+func example1b(b *testing.B) Day6 {
+	d, err := example1()
+	if err != nil {
+		b.Fatal(err)
+	}
+	return d
+}
+
+func example1t(t *testing.T) Day6 {
+	d, err := example1()
 	if err != nil {
 		t.Fatal(err)
 	}
 	return d
 }
 
-func example1(t *testing.T) Day6 {
-	return day6FromFile(t, "testdata/day6_example.txt")
+func example2() (Day6, error) {
+	return day6FromFile("testdata/day6_example2.txt")
 }
 
-func example2(t *testing.T) Day6 {
-	return day6FromFile(t, "testdata/day6_example2.txt")
+func example2t(t *testing.T) Day6 {
+	d, err := example2()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return d
+}
+
+func example2b(b *testing.B) Day6 {
+	d, err := example2()
+	if err != nil {
+		b.Fatal(err)
+	}
+	return d
 }
 
 func TestDay6OrbitC(t *testing.T) {
+	d := example1t(t)
 	want := "B"
-	d := example1(t)
 	got := d.Orbit("C")
 	if want != got {
 		t.Fatalf("want %q but got %q", want, got)
 	}
 }
 
+func BenchmarkDay6OrbitC(b *testing.B) {
+	d := example1b(b)
+	for i := 0; i < b.N; i++ {
+		d.Orbit("C")
+	}
+}
+
 func TestDay6OrbitCountCOM(t *testing.T) {
-	d := example1(t)
+	d := example1t(t)
 	want := 0
 	got := d.OrbitCount(COM)
 	if want != got {
@@ -41,7 +80,7 @@ func TestDay6OrbitCountCOM(t *testing.T) {
 }
 
 func TestDay6OrbitCountD(t *testing.T) {
-	d := example1(t)
+	d := example1t(t)
 	want := 3
 	got := d.OrbitCount("D")
 	if want != got {
@@ -50,7 +89,7 @@ func TestDay6OrbitCountD(t *testing.T) {
 }
 
 func TestDay6OrbitCountL(t *testing.T) {
-	d := example1(t)
+	d := example1t(t)
 	want := 7
 	got := d.OrbitCount("L")
 	if want != got {
@@ -59,7 +98,7 @@ func TestDay6OrbitCountL(t *testing.T) {
 }
 
 func TestDay6Example(t *testing.T) {
-	d := example1(t)
+	d := example1t(t)
 	want := 42
 	got := d.OrbitCountChecksum()
 	if want != got {
@@ -68,7 +107,10 @@ func TestDay6Example(t *testing.T) {
 }
 
 func TestDay6Part1(t *testing.T) {
-	d := day6FromFile(t, "testdata/day6.txt")
+	d, err := day6FromFile("testdata/day6.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 	want := 142497
 	got := d.OrbitCountChecksum()
 	if want != got {
@@ -76,8 +118,18 @@ func TestDay6Part1(t *testing.T) {
 	}
 }
 
+func BenchmarkDay6Part1(b *testing.B) {
+	d, err := day6FromFile("testdata/day6.txt")
+	if err != nil {
+		b.Fatal(err)
+	}
+	for i := 0; i < b.N; i++ {
+		d.OrbitCountChecksum()
+	}
+}
+
 func TestDay6CommonOrbit(t *testing.T) {
-	d := example2(t)
+	d := example2t(t)
 	want := "D"
 	got := d.CommonOrbit("YOU", "SAN")
 	if want != got {
@@ -85,8 +137,15 @@ func TestDay6CommonOrbit(t *testing.T) {
 	}
 }
 
+func BenchmarkDay6CommonOrbit(b *testing.B) {
+	d := example2b(b)
+	for i := 0; i < b.N; i++ {
+		d.CommonOrbit("YOU", "SAN")
+	}
+}
+
 func TestDay6Part2Example(t *testing.T) {
-	d := day6FromFile(t, "testdata/day6_example2.txt")
+	d := example2t(t)
 	want := 4
 	got := d.Transfers("YOU", "SAN")
 	if want != got {
@@ -95,7 +154,10 @@ func TestDay6Part2Example(t *testing.T) {
 }
 
 func TestDay6Part2(t *testing.T) {
-	d := day6FromFile(t, "testdata/day6.txt")
+	d, err := day6FromFile("testdata/day6.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 	want := 301
 	got := d.Transfers("YOU", "SAN")
 	if want != got {
