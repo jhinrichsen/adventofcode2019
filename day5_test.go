252fd70fb177b5473472c34eb427addb6a8e9fbc
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2020-02-24 22:13:06 +0100

    fix: working day 5 part 2

--- day5_test.go
+++ day5_test.go
@@ -1,5 +1,10 @@
 package adventofcode2019
 
+import (
+	"fmt"
+	"testing"
+)
+
 var day5Part1Examples = []struct {
 	in, out string
 }{
@@ -12,6 +17,8 @@ var day5Part1Examples = []struct {
 }
 
 const (
+	AirContitionerUnit        = 1
+	ThermalRadiatorController = 5
 
 	// The program will output 999 if the input value is below 8, output 1000
 	// if the input value is equal to 8, or output 1001 if the input value is
@@ -31,9 +38,9 @@ var day5Part2Examples = []struct {
 	{"3,9,8,9,10,9,4,9,99,-1,8", 8, 1, "8 == 8 ? 1 : 0 (position mode)"},
 	{"3,9,8,9,10,9,4,9,99,-1,8", 9, 0, "9 == 8 ? 1 : 0 (position mode)"},
 
-	{"3,9,7,9,10,9,4,9,99,-1,8", 0, 0, "0 < 8 ? 1 : 0 (position mode)"},
-	{"3,9,7,9,10,9,4,9,99,-1,8", 7, 0, "7 < 8 ? 1 : 0 (position mode)"},
-	{"3,9,7,9,10,9,4,9,99,-1,8", 8, 1, "8 < 8 ? 1 : 0 (position mode)"},
+	{"3,9,7,9,10,9,4,9,99,-1,8", 0, 1, "0 < 8 ? 1 : 0 (position mode)"},
+	{"3,9,7,9,10,9,4,9,99,-1,8", 7, 1, "7 < 8 ? 1 : 0 (position mode)"},
+	{"3,9,7,9,10,9,4,9,99,-1,8", 8, 0, "8 < 8 ? 1 : 0 (position mode)"},
 	{"3,9,7,9,10,9,4,9,99,-1,8", 9, 0, "9 < 8 ? 1 : 0 (position mode)"},
 
 	{"3,3,1108,-1,8,3,4,3,99", 0, 0, "0 == 8 ? 1 : 0 (immediate mode)"},
@@ -41,9 +48,9 @@ var day5Part2Examples = []struct {
 	{"3,3,1108,-1,8,3,4,3,99", 8, 1, "8 == 8 ? 1 : 0 (immediate mode)"},
 	{"3,3,1108,-1,8,3,4,3,99", 9, 0, "9 == 8 ? 1 : 0 (immediate mode)"},
 
-	{"3,3,1107,-1,8,3,4,3,99", 0, 0, "0 < 8 ? 1 : 0 (immediate mode)"},
-	{"3,3,1107,-1,8,3,4,3,99", 7, 0, "7 < 8 ? 1 : 0 (immediate mode)"},
-	{"3,3,1107,-1,8,3,4,3,99", 8, 1, "8 < 8 ? 1 : 0 (immediate mode)"},
+	{"3,3,1107,-1,8,3,4,3,99", 0, 1, "0 < 8 ? 1 : 0 (immediate mode)"},
+	{"3,3,1107,-1,8,3,4,3,99", 7, 1, "7 < 8 ? 1 : 0 (immediate mode)"},
+	{"3,3,1107,-1,8,3,4,3,99", 8, 0, "8 < 8 ? 1 : 0 (immediate mode)"},
 	{"3,3,1107,-1,8,3,4,3,99", 9, 0, "9 < 8 ? 1 : 0 (immediate mode)"},
 
 	{"3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", 0, 0, "0 == 0 ? 0 : 1 (position mode)"},
@@ -63,7 +70,6 @@ var day5Part2Examples = []struct {
 	{day5Part2LargeExample, 1000, 1001, "1000 < 8: 999, == 8: 1000, > 8: 1001"},
 }
 
-/* TODO
 func TestDay5Part1Examples(t *testing.T) {
 	for _, tt := range day5Part1Examples {
 		id := fmt.Sprintf("Day5(%s)", tt.in)
@@ -85,35 +91,15 @@ func channels() (chan int, chan int) {
 }
 
 func TestDay5Part1(t *testing.T) {
+	want := 16225258
 	in, out := channels()
 	lines, err := Lines("testdata/day5.txt")
 	if err != nil {
 		t.Fatal(err)
 	}
 	go Day5(MustSplit(lines[0]), in, out)
-	in <- 1    // ID for air conditioner unit
-	var dc int // diagnostic code
-
-	// we do not know when output finishes, so we cannot abort on the first
-	// non-0 value. Instead, keep a history accumulation
-	var rcs []int
-	for rc := range out {
-		dc = rc
-		rcs = append(rcs, rc)
-	}
-	// delete last entry, the diagnostic code
-	rcs = rcs[:len(rcs)-1]
-
-	// Check all rcs
-	for i, rc := range rcs {
-		if rc != 0 {
-			t.Fatalf("rc #%d: want 0 but got %d", i, rc)
-		}
-	}
-
-	// Check diagnostic code
-	want := 16225258
-	got := dc
+	in <- AirContitionerUnit
+	got := last(t, out)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
@@ -132,8 +118,41 @@ func TestIdentity(t *testing.T) {
 	}
 }
 
+// TestDay5Example2 runs a little program to check multiply.
+// For example, consider the program 1002,4,3,4,33.
+//
+//The first instruction, 1002,4,3,4, is a multiply instruction - the rightmost
+//two digits of the first value, 02, indicate opcode 2, multiplication. Then,
+//going right to left, the parameter modes are 0 (hundreds digit), 1 (thousands
+//digit), and 0 (ten-thousands digit, not present and therefore zero):
+//ABCDE
+// 1002
+//
+//DE - two-digit opcode,      02 == opcode 2
+// C - mode of 1st parameter,  0 == position mode
+// B - mode of 2nd parameter,  1 == immediate mode
+// A - mode of 3rd parameter,  0 == position mode,
+//                                  omitted due to being a leading zero
+//This instruction multiplies its first two parameters. The first parameter, 4
+//in position mode, works like it did before - its value is the value stored at
+//address 4 (33). The second parameter, 3 in immediate mode, simply has value
+//3. The result of this operation, 33 * 3 = 99, is written according to the
+//third parameter, 4 in position mode, which also works like it did before - 99
+//is written to address 4.
+func TestDay5Part2Example(t *testing.T) {
+	prog := MustSplit("1002,4,3,4,33")
+	in, out := channels()
+	Day5(prog, in, out)
+	// we would not be here if last code wasn't 99
+	want := 99
+	got := prog[4]
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
 func TestDay5Part2Examples(t *testing.T) {
-	for _, tt := range day5Part2Examples[1:2] {
+	for _, tt := range day5Part2Examples {
 		id := fmt.Sprintf("Day5(%s)", tt.description)
 		prog := MustSplit(tt.prog)
 		t.Run(id, func(t *testing.T) {
@@ -141,7 +160,13 @@ func TestDay5Part2Examples(t *testing.T) {
 			go Day5(prog, in, out)
 			in <- tt.in
 			want := tt.out
-			got := <-out
+			// one or multiple outputs that must be 0?
+			var got int
+			if tt.prog == day5Part2LargeExample {
+				got = last(t, out)
+			} else {
+				got = <-out
+			}
 			if want != got {
 				t.Fatalf("%s: want %d but got %d", id,
 					want, got)
@@ -150,11 +175,39 @@ func TestDay5Part2Examples(t *testing.T) {
 	}
 }
 
+func last(t *testing.T, c chan int) int {
+	var dc int // diagnostic code
+
+	// we do not know when output finishes, so we cannot abort on the first
+	// non-0 value. Instead, keep a history accumulation
+	var rcs []int
+	for rc := range c {
+		dc = rc
+		rcs = append(rcs, rc)
+	}
+	// delete last entry, the diagnostic code
+	rcs = rcs[:len(rcs)-1]
+
+	// Check all rcs
+	for i, rc := range rcs {
+		if rc != 0 {
+			t.Fatalf("rc #%d: want 0 but got %d", i, rc)
+		}
+	}
+	return dc
+}
+
 func TestDay5Part2(t *testing.T) {
 	want := 2808771
-	got := -1
+	in, out := channels()
+	lines, err := Lines("testdata/day5.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	go Day5(MustSplit(lines[0]), in, out)
+	in <- ThermalRadiatorController
+	got := <-out
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
-*/
