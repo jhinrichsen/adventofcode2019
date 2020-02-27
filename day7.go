10cca2c4f6ee43fef27bf5f9924a5af80d8a0ee2
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2020-02-27 23:08:11 +0100

    feat: day 7 part 1

---
+++ day7.go
@@ -0,0 +1,89 @@
+package adventofcode2019
+
+import (
+	"math/rand"
+)
+
+// Day7Part1 returns max thrust for given program, using a permutation of all
+// phases.
+func Day7Part1(prog IntCode, phases string) int {
+	max := 0
+	perms := permutations(phases)
+	for perm := range perms {
+		// fmt.Printf("calculating permutation %q\n", perm)
+		// convert "0" -> 0, "1" -> 1, ...
+		buf := []byte(perm)
+		for i := 0; i < len(buf); i++ {
+			buf[i] -= '0'
+		}
+		in, out := amps(prog, buf)
+		// fmt.Printf("0 -> %+v\n", in)
+		in <- 0
+		// fmt.Printf("waiting for %+v\n", out)
+		n := <-out
+		// fmt.Printf("read %d from %+v\n", n, out)
+		// fmt.Printf("permutation %q returns %d\n", perm, n)
+		if n > max {
+			max = n
+		}
+	}
+	return max
+}
+
+func amps(prog IntCode, phases []byte) (chan<- int, <-chan int) {
+	var proc IntCodeProcessor = Day5
+	var connect, lastConnect chan int
+	input, output := channels()
+	// fmt.Printf("created input=%+v, output=%+v\n", input, output)
+	for i, phase := range phases {
+		if i == 0 {
+			connect = make(chan int)
+			lastConnect = connect
+			// fmt.Printf("created connect=%+v\n", connect)
+			// fmt.Printf("creating first amp %+v -> %+v\n",
+			// input, connect)
+			go proc(prog.Copy(), input, connect)
+			input <- int(phase)
+			// fmt.Printf("amp %d init: %d -> %+v\n",
+			// i, int(phase), input)
+		} else if i+1 == len(phases) {
+			// fmt.Printf("creating last amp %+v -> %+v\n",
+			// lastConnect, output)
+			go proc(prog.Copy(), lastConnect, output)
+			lastConnect <- int(phase)
+			// fmt.Printf("amp %d init: %d -> %+v\n",
+			// i, int(phase), lastConnect)
+		} else {
+			connect = make(chan int)
+			// fmt.Printf("creating amp #%d: %+v -> %+v\n",
+			// i, lastConnect, connect)
+			go proc(prog.Copy(), lastConnect, connect)
+			lastConnect <- int(phase)
+			// fmt.Printf("amp %d init: %d -> %+v\n",
+			// i, int(phase), lastConnect)
+			lastConnect = connect
+		}
+	}
+	return input, output
+}
+
+func permutations(s string) map[string]bool {
+	m := make(map[string]bool, fac(len(s)))
+	// Heap or Yates would be better but require more coding
+	buf := []byte(s)
+	for len(m) < fac(len(s)) {
+		rand.Shuffle(len(s), func(i, j int) {
+			buf[i], buf[j] = buf[j], buf[i]
+		})
+		m[string(buf)] = true
+	}
+	return m
+}
+
+func fac(n int) int {
+	f := 1
+	for i := 2; i <= n; i++ {
+		f *= i
+	}
+	return f
+}
---
+++ day7_test.go
@@ -0,0 +1,58 @@
+package adventofcode2019
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+const (
+	prog1 = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
+	prog2 = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23," +
+		"23,4,23,99,0,0"
+	prog3 = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002," +
+		"33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
+)
+
+var day7Part1Examples = []struct {
+	phases string
+	prog   string
+	want   int
+}{
+	{"43210", prog1, 43210},
+	{"01234", prog2, 54321},
+	{"10432", prog3, 65210},
+}
+
+func TestDay7Part1Examples(t *testing.T) {
+	for _, tt := range day7Part1Examples {
+		id := fmt.Sprintf("Day7(%s, %s)", "prog1", tt.phases)
+		t.Run(id, func(t *testing.T) {
+			got := Day7Part1(MustSplit(tt.prog), tt.phases)
+			if tt.want != got {
+				t.Fatalf("%s: want %d but got %d", id,
+					tt.want, got)
+			}
+		})
+	}
+}
+
+func TestFac(t *testing.T) {
+	want := 120
+	got := fac(5)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay7Part1(t *testing.T) {
+	buf, err := ioutil.ReadFile("testdata/day7.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := 24405
+	got := Day7Part1(MustSplit(string(buf)), "01234")
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
