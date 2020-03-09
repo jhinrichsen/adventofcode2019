069dbd3f459e99738caab4c2561113338392a912
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2020-03-09 19:40:30 +0100

    feat: add day 10 part 1

--- day10.go
+++ day10.go
@@ -1,6 +1,32 @@
 package adventofcode2019
 
-// Day10 holds a map of asteroids.
+import (
+	"math"
+)
+
+// Polar converts cartesian into polar coordinates
+// https://de.wikipedia.org/wiki/Polarkoordinaten#Umrechnung_von_kartesischen_Koordinaten_in_Polarkoordinaten
+func Polar(x, y int) (float64, float64) {
+	r := math.Sqrt(float64(x*x + y*y))
+	fx := float64(x)
+	fy := float64(y)
+	var φ float64
+
+	if x > 0 && y >= 0 {
+		φ = math.Atan(fy / fx)
+	} else if x > 0 && y < 0 {
+		φ = math.Atan(fy/fx) + 2*math.Pi
+	} else if x < 0 {
+		φ = math.Atan(fy/fx) + math.Pi
+	} else if x == 0 && y > 0 {
+		φ = math.Pi / 2
+	} else /* x == 0 && y < 0 */ {
+		φ = 3 * math.Pi / 2
+	}
+	return r, φ
+}
+
+// Day10 holds a cartesian map of asteroids.
 type Day10 struct {
 	asteroids []Asteroid
 }
@@ -8,6 +34,11 @@ type Day10 struct {
 // An Asteroid is identified by a dimensionless (x/y) position in a 2D space.
 type Asteroid struct {
 	x, y int
+}
+
+// Distance returns the cartesian distance (dx, dy) from a to a2.
+func (a Asteroid) Distance(a2 Asteroid) (int, int) {
+	return a2.x - a.x, a2.y - a.y
 }
 
 // NewDay10 parses newline separated strings into a Day 10 struct.
@@ -47,4 +78,40 @@ func NewDay10(asteroids []byte) Day10 {
 		}
 	}
 	return d
+}
+
+// Best returns the asteroid that can see most asteroids, and the number of
+// visible asteroids.
+func (a Day10) Best() (Asteroid, int) {
+	var best Asteroid
+	var maxVisible int
+	// convert relative distance (dx/dy) into (phi, len)
+	for i := range a.asteroids {
+		// map of angles and one corresopnding min distance
+		visible := make(map[float64]float64, len(a.asteroids))
+		for j := range a.asteroids {
+			// skip ourself
+			if i == j {
+				continue
+			}
+			r, φ := Polar(a.asteroids[i].Distance(a.asteroids[j]))
+			// already an asteroid at same angle?
+			if l, ok := visible[φ]; ok {
+				// are we closer?
+				if r < l {
+					// yes, make us visible, hide other
+					visible[φ] = r
+				}
+			} else {
+				// no, just save us
+				visible[φ] = r
+			}
+		}
+		// found a better planet?
+		if len(visible) > maxVisible {
+			best = a.asteroids[i]
+			maxVisible = len(visible)
+		}
+	}
+	return best, maxVisible
 }
--- day10_test.go
+++ day10_test.go
@@ -1,18 +1,94 @@
 package adventofcode2019
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
 	"testing"
 )
 
-func TestExample1(t *testing.T) {
-	example1 := []byte(`
+func TestPolar(t *testing.T) {
+	r, φ := Polar(1, 1)
+	log.Printf("(%d,%d) -> r=%v,=%v\n", 1, 1, r, φ)
+	r, φ = Polar(1, -1)
+	r, φ = Polar(-1, -1)
+	r, φ = Polar(-1, 1)
+}
+
+const (
+	day10Example1 = `
 	.#..#
 	.....
 	#####
 	....#
 	...##
-	`)
-	d := NewDay10(example1)
+	`
+
+	day10Example2 = `
+	......#.#.
+	#..#.#....
+	..#######.
+	.#.#.###..
+	.#..#.....
+	..#....#.#
+	#..#....#.
+	.##.#..###
+	##...#..#.
+	.#....####
+	`
+
+	day10Example3 = `
+	#.#...#.#.
+	.###....#.
+	.#....#...
+	##.#.#.#.#
+	....#.#.#.
+	.##..###.#
+	..#...##..
+	..##....##
+	......#...
+	.####.###.
+	`
+
+	day10Example4 = `
+	.#..#..###
+	####.###.#
+	....###.#.
+	..###.##.#
+	##.##.#.#.
+	....###..#
+	..#.#..#.#
+	#..#.#.###
+	.##...##.#
+	.....#.#..
+	`
+
+	day10Example5 = `
+	.#..##.###...#######
+	##.############..##.
+	.#.######.########.#
+	.###.#######.####.#.
+	#####.##.#.##.###.##
+	..#####..#.#########
+	####################
+	#.####....###.#.#.##
+	##.#################
+	#####.##.###..####..
+	..######..##.#######
+	####.##.####...##..#
+	.#####..#.######.###
+	##...#.##########...
+	#.##########.#######
+	.####.#.###.###.#.##
+	....##.##.###..#####
+	.#.#.###########.###
+	#.#.#.#####.####.###
+	###.##.####.##.#..##
+	`
+)
+
+func TestExample1(t *testing.T) {
+	d := NewDay10([]byte(day10Example1))
 
 	// Check number of asteroids
 	if len(d.asteroids) != 10 {
@@ -24,10 +100,65 @@ func TestExample1(t *testing.T) {
 			second, d.asteroids[1])
 
 	}
-	a8 := Asteroid{3, 4}
-	if d.asteroids[8] != a8 {
-		t.Fatalf("expected asteroid %+v at index 1, got %+v",
-			a8, d.asteroids[8])
+}
 
+var day10Examples = []struct {
+	asteroidMap string
+	best        Asteroid
+	bestCount   int
+}{
+	{day10Example1, Asteroid{3, 4}, 8},
+	{day10Example2, Asteroid{5, 8}, 33},
+	{day10Example3, Asteroid{1, 2}, 35},
+	{day10Example4, Asteroid{6, 3}, 41},
+	{day10Example5, Asteroid{11, 13}, 210},
+}
+
+func TestDay10Examples(t *testing.T) {
+	for i, tt := range day10Examples {
+		id := fmt.Sprintf("Day10Part1 example #%d", i+1)
+		t.Run(id, func(t *testing.T) {
+			d := NewDay10([]byte(tt.asteroidMap))
+			gotA, got := d.Best()
+			if tt.best != gotA {
+				t.Fatalf("%d: want %+v but got %+v",
+					tt.best.x, tt.best.y, gotA)
+			}
+			if tt.bestCount != got {
+				t.Fatalf("want %d but got %d", tt.bestCount, got)
+			}
+		})
+	}
+}
+
+func TestDay10Part1(t *testing.T) {
+	filename := "testdata/day10.txt"
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	d := NewDay10(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := 267
+	_, got := d.Best()
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func BenchmarkDay10Part1(b *testing.B) {
+	filename := "testdata/day10.txt"
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		b.Fatal(err)
+	}
+	d := NewDay10(buf)
+	if err != nil {
+		b.Fatal(err)
+	}
+	for i := 0; i < b.N; i++ {
+		d.Best()
 	}
 }
