78a7814fd20362a30d1a03fc392930cc0a357e22
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2020-03-14 21:43:47 +0100

    feat: add day 11 part 2

--- day11.go
+++ day11.go
@@ -12,37 +12,58 @@ const (
 	colorWhite = 1
 )
 
-// RegistrationIdentifier is a 2D map of black (false) and white (true)
-type RegistrationIdentifier map[complex128]bool
+// RegistrationID is a 2D map of black (false) and white (true)
+type RegistrationID map[complex128]bool
 
-func (a RegistrationIdentifier) dim() (complex128, complex128) {
-	return -1 + -1i, 1 + 1i
+// box returns the integer away from 0 that can hold f.
+func box(f float64) int {
+	return int(math.Round(math.Ceil(f)))
+}
+
+// returns min and max as 2D coordinates (x/y)
+func (a RegistrationID) dim() (min, max Point) {
+	minX, minY := math.MaxInt64, math.MaxInt64
+	maxX, maxY := -math.MaxInt64, -math.MaxInt64
+	for k := range a {
+		x := box(real(k))
+		if x < minX {
+			minX = x
+		} else if x > maxX {
+			maxX = x
+		}
+		y := box(imag(k))
+		if y < minY {
+			minY = y
+		} else if y > maxY {
+			maxY = y
+		}
+	}
+	return Point{minX - 2, minY - 2}, Point{maxX + 2, maxY + 2}
 }
 
 // pbm creats an image in portable bitmap format from an registrationIdentifier.
 // https://en.wikipedia.org/wiki/Netpbm#File_formats
-func (a RegistrationIdentifier) pbm() []byte {
+func (a RegistrationID) pbm() []byte {
 	var buf bytes.Buffer
 	// magic number
 	fmt.Fprintln(&buf, "P1")
 	min, max := a.dim()
-	box := func(f float64) int {
-		return int(math.Round(math.Ceil(f)))
-	}
-	x0 := box(real(min))
-	xn := box(real(max))
-	y0 := box(imag(min))
-	yn := box(imag(max))
 	// width height
-	fmt.Fprintf(&buf, "%d %d\n", xn-x0, yn-y0)
-	for y := y0; y < yn; y++ {
-		for x := x0; x < xn; x++ {
+	fmt.Fprintf(&buf, "%d %d\n", max.x-min.x, max.y-min.y)
+	for y := min.y; y < max.y; y++ {
+		for x := min.x; x < max.x; x++ {
 			// PBM uses 0 for white and 1 for black (inverse)
 			var pbmCol int
-			if _, white := a[complex(float64(x), float64(y))]; white {
-				pbmCol = colorBlack
+			if b, ok := a[complex(float64(x), float64(y))]; ok {
+				if b {
+
+					pbmCol = colorWhite
+				} else {
+					pbmCol = colorBlack
+				}
 			} else {
-				pbmCol = colorWhite
+				// no color, default
+				pbmCol = colorBlack
 			}
 			fmt.Fprintf(&buf, "%d", pbmCol)
 		}
@@ -53,21 +74,23 @@ func (a RegistrationIdentifier) pbm() []byte {
 
 // Day11Part1 returns the number of painted tiles.
 func Day11Part1(prog IntCode) int {
-	return len(registrationIdentifier(prog))
+	return len(newRegistrationID(prog, colorBlack))
 }
 
-func registrationIdentifier(prog IntCode) RegistrationIdentifier {
-	const (
-		// no types because we want to pass it to the IntCode Computer
-		turnLeft  = 0
-		turnRight = 1
-	)
+// Day11Part2 returns a PBM encoded registration ID.
+// The image is horizontally flipped so em well you can read it from the inside
+// of the transparent hull?
+func Day11Part2(prog IntCode) []byte {
+	return newRegistrationID(prog, colorWhite).pbm()
+}
+
+func newRegistrationID(prog IntCode, initialColor int) RegistrationID {
 	in, out := channels()
 
 	// boot emergency hull painting robot
 	go Day5(prog, in, out)
 
-	panels := make(RegistrationIdentifier)
+	panels := make(RegistrationID)
 	position := 0 + 0i
 	left := 0 + 1i
 	right := 0 - 1i
@@ -86,6 +109,11 @@ func registrationIdentifier(prog IntCode) RegistrationIdentifier {
 		panels[position] = b
 	}
 	translateTurn := func(i int) complex128 {
+		const (
+			// no types because we want to pass it to the IntCode Computer
+			turnLeft  = 0
+			turnRight = 1
+		)
 		if i == turnLeft {
 			return left
 		} else if i == turnRight {
@@ -95,12 +123,11 @@ func registrationIdentifier(prog IntCode) RegistrationIdentifier {
 		}
 	}
 
-	in <- color()
+	in <- initialColor
 	for col := range out {
 		setColor(col)
-		turnValue := <-out
 		// make robot turn
-		turn := translateTurn(turnValue)
+		turn := translateTurn(<-out)
 		direction *= turn
 		// one step
 		position += direction
--- day11_test.go
+++ day11_test.go
@@ -3,7 +3,6 @@ package adventofcode2019
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
 	"testing"
 )
 
@@ -12,18 +11,19 @@ func TestDay11Pbm(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := registrationIdentifier(MustSplit(string(buf)))
-	fmt.Fprintln(os.Stdout, string(got.pbm()))
+	got := newRegistrationID(MustSplit(string(buf)), colorBlack)
+	fmt.Println("Day 11, part 1")
+	fmt.Println(string(got.pbm()))
 }
 
 func TestDay11Part1(t *testing.T) {
+	// First try want := 9870
+	// Second try want := 907
+	want := 2343
 	buf, err := ioutil.ReadFile("testdata/day11.txt")
 	if err != nil {
 		t.Fatal(err)
 	}
-	// First try want := 9870
-	// Second try want := 907
-	want := 2343
 	got := Day11Part1(MustSplit(string(buf)))
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
@@ -42,4 +42,14 @@ func TestMapOfBools(t *testing.T) {
 	if len(m) != 1 {
 		t.Fatalf("want 1 map entry but got %d", len(m))
 	}
+}
+
+func TestDay11Part2(t *testing.T) {
+	buf, err := ioutil.ReadFile("testdata/day11.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day11Part2(MustSplit(string(buf)))
+	fmt.Println("Day 11, part 2:")
+	fmt.Println(string(got))
 }
