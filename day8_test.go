8ec0af1cdb237ea04bb9f2bf706e9fabb54cc95d
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2020-03-14 07:31:08 +0100

    test: automatic testing

--- day8_test.go
+++ day8_test.go
@@ -1,8 +1,11 @@
 package adventofcode2019
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"reflect"
 	"testing"
 )
 
@@ -36,33 +39,46 @@ func BenchmarkDay8Part1(b *testing.B) {
 	}
 }
 
+func day8Part2Result() ([]byte, error) {
+	filename := "testdata/day8-part2-result.txt"
+	return ioutil.ReadFile(filename)
+}
+
 func TestDay8Part2(t *testing.T) {
 	filename := "testdata/day8.txt"
 	digits, err := ioutil.ReadFile(filename)
 	if err != nil {
 		t.Fatal(err)
 	}
-	_ = "GKCKH"
-	// well, this is ASCII art, so in absence of a package that can parse it
-	// we need human interaction
+	want, err := day8Part2Result()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	got, err := Day8Part2(digits)
 	if err != nil {
 		t.Fatal(err)
 	}
+	// well, this is ASCII art, so in absence of a package that can parse it
+	// we need human interaction
+	var humanReadable bytes.Buffer
 	for i := 0; i < len(got); i++ {
 		if got[i] == '0' {
-			fmt.Printf("%s", string("  "))
+			fmt.Fprintf(&humanReadable, "%s", string("  "))
 		} else if got[i] == '1' {
-			fmt.Printf("%s", string("X "))
+			fmt.Fprintf(&humanReadable, "%s", string("X "))
 		} else {
-			fmt.Printf("%s", string("? "))
-			// t.Fatalf("bad color at index %d: %d", i, got[i])
+			fmt.Fprintf(&humanReadable, "%s", string("? "))
 		}
 		if i%25 == 24 {
-			fmt.Println()
+			fmt.Fprintln(&humanReadable)
 		}
 	}
-	fmt.Println()
+	if !reflect.DeepEqual(want, humanReadable.Bytes()) {
+		fmt.Fprintf(os.Stderr, "want: %v\n", want)
+		fmt.Fprintf(os.Stderr, " got: %v\n", humanReadable.Bytes())
+		t.Fatal("want does not equal got")
+	}
 }
 
 func BenchmarkDay8Part2(b *testing.B) {
